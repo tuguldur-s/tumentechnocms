f579e39b72090d6bd9421b42bd9c98eb6ab03a41
otgoo1994 <62593887+otgoo1994@users.noreply.github.com>
2023-04-23 17:14:25 +0800

    changed db connection string

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,10 +1,27 @@
 ﻿const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: 'localhost',
-  database: 'icbc',
-  user: 'root',
-  password: ''
-});
+var os = require("os");
+let obj = {};
+if (os.hostname() === 'DESKTOP-AQH5C0N') {
+  obj = {
+    host: 'localhost',
+    database: 'icbc',
+    user: 'root',
+    password: ''
+  };
+} else {
+  obj = {
+    host: 'db-mysql-sgp1-66799-do-user-11118497-0.b.db.ondigitalocean.com',
+    database: 'icbc',
+    user: 'testadmin',
+    port: 25060,
+    ssl: {
+      //ca: fs.readFileSync("./public/contents/ca-certificate.crt"),
+      ca: fs.readFileSync("./public/contents/ca-certificate.crt"),
+    },
+    password: 'AVNS_FvgreVmM7HlicMArR2V'
+  }
+}
+const db = mysql.createConnection(obj);
 
 // const db = mysql.createConnection({
 //   host: 'localhost',
